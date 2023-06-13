ea6144047013103213a1a119c9eb9cf271c81d03
Kharbega Soumia <109501749+Kharbega@users.noreply.github.com>
2023-06-13 20:43:50 +0200

    Update product.js

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -1,4 +1,6 @@
+//PAGE PRODUIT//
 
+// identification des balises //
 
 const section = document.querySelector("body > main > div > section")
 const itemImg = document.querySelector("body > main > div > section > article > div.item__img")
@@ -6,23 +8,19 @@ const productName = document.querySelector("#title")
 const productPrice = document.getElementById('price')
 const productDescription = document.getElementById('description')
 const productColors = document.querySelector("#colors")
-//console.log(productColors);
 
-//  Récupérer l’id du produit à affiche //
+//  Récupérer l’id du produit à afficher //
 
 let params = new URLSearchParams(document.location.search);
 let id = params.get("id");
 
 const url = `http://localhost:3000/api/products/${id}`
 
-// FIN  Récupérer l’id du produit à affiche //
+// FIN  Récupérer l’id du produit à afficher //
 
 
 //Insérer un produit et ses détails dans la page//
 
-
-
-//function getAllProducts() {//
 
 function getOneProduct() {
   fetch(url)
@@ -54,13 +52,11 @@ getOneProduct()
 
 
 
+// identification des balises //
 
 const addToCart = document.querySelector("#addToCart")
 
-
-
-
-
+//*********************Objet Produit qu'on va stocker dans le localstorage  & EVEN AU CLICK*/
 addToCart.addEventListener("click", (e) => {
   let ProductStorage = {
     idProduct: id,
@@ -68,21 +64,17 @@ addToCart.addEventListener("click", (e) => {
     colorProduct: document.querySelector("#colors").value,
   }
    e.preventDefault()
-  //console.log(localStorage);
-  //console.log('quantity');
 
 
-
-  //* / recupere la valeur //
+  //* On recupere la valeur //
    let ArrayStorage = JSON.parse(localStorage.getItem("addToCart")) || []
 
+   //********************On trouve le produit qui a le meme id et la meme couleur  */
       let foundProduct = ArrayStorage.find((item)=> ProductStorage.idProduct == item.idProduct && item.colorProduct == ProductStorage.colorProduct ) ;
-     // let foundColor = ArrayStorage.find ((itemcolor) => ProductStorage.colorProduct == itemcolor.colorProduct);
    
-  
+  //******************* Si un produit avec le meme id et color ,on additionne seulement les quantitees*/
       if ( foundProduct != undefined){ 
      foundProduct.quantity =  parseInt(foundProduct.quantity) + parseInt (ProductStorage.quantity);
-     console.log(ArrayStorage);
 
       localStorage.setItem("addToCart", JSON.stringify(ArrayStorage)) 
       
@@ -93,11 +85,56 @@ addToCart.addEventListener("click", (e) => {
      localStorage.setItem("addToCart", JSON.stringify(ArrayStorage))  
      
 } 
+/*console.log(foundProduct)
+console.log(foundProduct.quantity)
 
-/* if (foundProduct.quantity < 100 ) {
-    alert("Quantite maximum atteinte")
+let add = parseInt(foundProduct.quantity) + parseInt(ProductStorage.quantity)
+console.log(add)
+if (add > 100) {
+  ArrayStorage.quantity = 100;
+  let max = 100 - parseInt(foundProduct.quantity);
+
+console.log(max)
+  if (max > 100) {
     
+    alert('Quantité maximale atteinte !');
+  }
+} else {
+  ArrayStorage.quantity =max ;
+}
+
+ArrayStorage.forEach((product) => {
+  if (product.idProduct == ProductStorage.idProduct && product.colorProduct == ProductStorage.colorProduct) {
+    if (add <= 100) {
+      product.quantity = parseInt(product.quantity) + parseInt(ProductStorage.quantity);
+    } else {
+      product.quantity = 100;
+    } 
+  }else {
+ArrayStorage.push(product)
+
+localStorage.setItem("addToCart", JSON.stringify(ArrayStorage)) 
+window.location.href = 'index.html';
+window.localStorage.clear()
+}
+/* else {
+// test le/les champs qui n'ont pas été renseigné.
+testContentFields(ProductStorage.quantity, ProductStorage.quantity)
+
+
+  }
+
   
+});*/
+
+/* for (p =0; p < ArrayStorage.length;p++) {
+
+ if (`${ArrayStorage[length].quantity }` < 100  ) {
+  
+  
+    alert(`Nombre maximum d'articles atteint  ${ArrayStorage[length].quantity }`)
+ }
+
 }; */
 }); 
 
